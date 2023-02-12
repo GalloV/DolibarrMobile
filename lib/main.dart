d8302b88e73abc3358a699242a76a3c38c31264b
maremediop <maremediop1@esp.sn>
2023-02-12 05:02:14 +0000

    mareme_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:dolibarr_mobile/utils.dart';
+import 'package:dolibarr_mobile/page-1/iphone-13-pro-max-2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +15,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: MyCustomScrollBehavior(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Helloi'),
+        body: SingleChildScrollView(
+          child: Scene(),
         ),
       ),
     );
